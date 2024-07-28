75facfc892ad28693e014d17b1c02759472ea9cd
EliaukRender <chang_showing@163.com>
2024-07-28 17:30:21 +0800

    feat

--- src/views/eliaukManage/index.jsx
+++ src/views/eliaukManage/index.jsx
@@ -1,11 +1,19 @@
 import React, { memo } from 'react';
 import { EliaukManageStyles } from '@/views/eliaukManage/styles';
 
+const eliaukManageUrl = 'http://47.113.177.51:50001';
+
 /**
  * @description: 后台管理系统
  */
 const EliaukManage = () => {
-	return <EliaukManageStyles></EliaukManageStyles>;
+	return (
+		<EliaukManageStyles>
+			<div className='iframe-container'>
+				<iframe className='iframe' src={eliaukManageUrl}></iframe>
+			</div>
+		</EliaukManageStyles>
+	);
 };
 
 export default memo(EliaukManage);
--- src/views/eliaukManage/styles.js
+++ src/views/eliaukManage/styles.js
@@ -1,9 +1,27 @@
 import styled from 'styled-components';
+import bg from '@/assets/image/fullImgs/bg-1.png';
 
 export const EliaukManageStyles = styled.div`
 	width: 100%;
 	height: 100vh;
-	padding-top: 70px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	box-sizing: border-box;
-	background-color: lightcyan;
+	padding-top: 70px;
+	overflow: hidden;
+	background-image: url(${bg});
+	background-size: cover;
+	background-position: center;
+
+	.iframe-container {
+		width: 100%;
+		height: 100%;
+
+		.iframe {
+			width: 100%;
+			height: 100%;
+			border: none;
+		}
+	}
 `;
