d58442f75438b6a72c39924c4b5d9cda93ab65ce
EliaukRender <chang_showing@163.com>
2024-08-01 22:25:04 +0800

    修改iframe比例

--- src/views/eliaukManage/styles.js
+++ src/views/eliaukManage/styles.js
@@ -17,10 +17,13 @@ export const EliaukManageStyles = styled.div`
 	.iframe-container {
 		width: 100%;
 		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 
 		.iframe {
-			width: 100%;
-			height: 100%;
+			width: 92%;
+			height: 95%;
 			border: none;
 		}
 	}
